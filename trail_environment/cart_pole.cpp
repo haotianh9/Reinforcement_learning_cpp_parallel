39604dfb40e78f272eda2a99f38dc81098e9f823
Haotian Hang <hanghaotian@gmail.com>
2021-11-09 13:53:11 -0800

    updated still two process

--- trail_environment/cart_pole.cpp
+++ trail_environment/cart_pole.cpp
@@ -129,14 +129,16 @@ inline void NN_run(int nproc){
 }
 int main(int argc, char**argv)
 {
-  
+  int nprocs;
   int myid;
   int n;
   MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  printf("There are %d processes running in this MPI program\n", nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-
+  
   if (myid == 0) {
-    NN_run(2);
+    NN_run(nprocs);
   }
   else {
     env_run(myid);
