3dfe73f1da3d705b87b749efb46a029db25e46b2
Haotian Hang <hanghaotian@gmail.com>
2021-11-09 23:32:06 -0800

    update multi process implementation(haven't debug yet)

--- trail_multi_environment/cart_MPI.h
+++ trail_multi_environment/cart_MPI.h
@@ -1,16 +1,16 @@
 #include "mpi.h"
 #define NNnode 0
+
 MPI_Status status;
-// MPI_Comm comm_action;
-// MPI_Comm comm_observation;
-// MPI_Comm comm_reward;
 MPI_Request request;
 
+int nprocs;
 
 const int control_vars = 1; // force along x
-const int state_vars = 6; // x, vel, angvel, angle, cosine, sine
-double dbuf[state_vars];
+const int obs_vars = 6; // x, vel, angvel, angle, cosine, sine
+
 double dbufa[control_vars];
-double dbufsrt[state_vars+2];
-const int Nepisodes=2;
+double dbufsrt[obs_vars+2]; // the dbuf is sending these things: state, reward, whether an episode terminate or done  or not (if terminate 1, if done 2, else 0) (terminate means fail; done means reach maximum time step in a period, thus need to add value function)
+const int Nepisodes=9;
 const int N_timestep=9;//Maximum time step in one episode
+const int Max_timestep=209;//Maximum training timestep (either define this or define Nepisodes)
--- trail_multi_environment/cart_pole.cpp
+++ trail_multi_environment/cart_pole.cpp
@@ -7,7 +7,7 @@
 //
 
 // compile: mpic++  -o cart cart_pole.cpp -fopenmp
-// running: mpirun -n 2 ./cart
+// running: mpirun -n N ./cart
 
 #include "cart_pole.h"
 #include "cart_MPI.h"
@@ -19,117 +19,88 @@
 inline void env_run(int myid)
 {
   
-//   comm->setStateActionDims(state_vars, control_vars);
 
   //OPTIONAL: action bounds
   bool bounded = true;
   std::vector<double> upper_action_bound{10}, lower_action_bound{-10};
-//   comm->setActionScales(upper_action_bound, lower_action_bound, bounded);
-
-  /*
-    // ALTERNATIVE for discrete actions:
-    vector<int> n_options = vector<int>{2};
-    comm->set_action_options(n_options);
-    // will receive either 0 or 1, app chooses resulting outcome
-  */
-
-  //OPTIONAL: hide state variables. e.g. show cosine/sine but not angle
-  // std::vector<bool> b_observable = {true, true, true, false, true, true};
-  //std::vector<bool> b_observable = {true, false, false, false, true, true};
-//   comm->setStateObservable(b_observable);
-  //comm->setIsPartiallyObservable();
 
   CartPole env;
 
   // while(true) //train loop
-  for (int i=0;i<Nepisodes;i++)
+  for (int i=0;i<(int) Nepisodes/nprocs;i++)
   {
     env.reset(); // prng with different seed on each process
-    //send initial state
-    std::vector<double> state = env.getState();
-    // for (int i=0;i<state_vars;i++) dbuf[i]=state[i];
-    std::copy(state.begin(), state.end(), dbuf);
-    MPI_Send(dbuf, state_vars, MPI_DOUBLE, NNnode, myid, MPI_COMM_WORLD);
-    
-    printf("%d send state = %f %f %f %f %f %f \n",myid, state[0] ,state[1], state[2] , state[3] ,state[4], state[5]);
+    //send initial obs
+    std::vector<double> obs = env.getobs();
+    // for (int i=0;i<obs_vars;i++) dbuf[i]=obs[i];
+    std::copy(obs.begin(), obs.end(), dbufsrt);
+    dbufsrt[obs_vars]=0;
+    dbufsrt[obs_vars+1]=0;
+    MPI_Send(dbufsrt, obs_vars+2, MPI_DOUBLE, NNnode, myid, MPI_COMM_WORLD);
+    printf("%d send obs = %f %f %f %f %f %f \n",myid, obs[0] ,obs[1], obs[2] , obs[3] ,obs[4], obs[5]);
     
 
     // while (true) //simulation loop
     for (int j=0; j <N_timestep; j++)
     {
-      // MPI_Irecv(dbufa, control_vars, MPI_DOUBLE, 1, 10, MPI_COMM_WORLD, &request);
-      // MPI_Wait( &request, &status);
-      MPI_Recv(dbufa, control_vars, MPI_DOUBLE, NNnode, myid, MPI_COMM_WORLD, &status); // recieve action
-      // std::vector<double> action(control_vars);
-      // for (int i=0;i<control_vars;i++) action[i]=dbufa[i];
+      MPI_Recv(dbufa, control_vars, MPI_DOUBLE, NNnode, myid*2, MPI_COMM_WORLD, &status); // recieve action
       std::vector<double> action(dbufa,dbufa+control_vars);
       printf("%d recieve action = %f  \n", myid, action[0]);
-      // if(comm->terminateTraining()) return; // exit program
-
-      bool poleFallen = env.advance(action); //advance the simulation:
-
-      std::vector<double> state = env.getState(); 
+      bool terminate = env.advance(action); //advance the simulation:
+      std::vector<double> obs = env.getobs(); 
       double reward = env.getReward();
       // send new observation, reward, and whether terminate or not, if terminate send 1, if not send 0
-      // for (int i=0;i<state_vars;i++) dbufsrt[i]=state[i];
-      std::copy(state.begin(), state.end(), dbufsrt);
-      dbufsrt[state_vars]=reward;
-      if(poleFallen){
-        dbufsrt[state_vars+1]=1;
-        MPI_Send(dbufsrt, state_vars+2, MPI_DOUBLE, NNnode, myid, MPI_COMM_WORLD);  
+      // for (int i=0;i<obs_vars;i++) dbufsrt[i]=obs[i];
+      std::copy(obs.begin(), obs.end(), dbufsrt);
+      dbufsrt[obs_vars+1]=reward;
+      if(terminate){
+        dbufsrt[obs_vars+1]=1;
+        MPI_Send(dbufsrt, obs_vars+2, MPI_DOUBLE, NNnode, myid, MPI_COMM_WORLD);  
         break;
+      }else if (j == N_timestep){
+        dbufsrt[obs_vars+1]=2;
+        MPI_Send(dbufsrt, obs_vars+2, MPI_DOUBLE, NNnode, myid, MPI_COMM_WORLD);  
       }else{
-        dbufsrt[state_vars+1]=0;
-        MPI_Send(dbufsrt, state_vars+2, MPI_DOUBLE, NNnode, myid, MPI_COMM_WORLD);  
+        dbufsrt[obs_vars+1]=0;
+        MPI_Send(dbufsrt, obs_vars+2, MPI_DOUBLE, NNnode, myid, MPI_COMM_WORLD);  
       }
     }  //end of simulation loop
   }// end of train loop
 }
-inline void NN_run(int nproc){
-  for (int i=0;i<Nepisodes;i++)
-    {
-      
-      // recieve intial state
+inline void NN_run(){
+  int n_ep=0;
+  int n_timestep=0;
+  bool done;
+  bool terminate;
+  while(true){
+    for (int i=1;i<=nprocs;i++){
+      MPI_Recv(dbufsrt, obs_vars+3, MPI_DOUBLE, i, i, MPI_COMM_WORLD, &status);
+      std::vector<double> obs(dbufsrt,dbufsrt+obs_vars);
+    ` std::vector<double> action =getAction(obs,control_vars);
+      std::copy(action.begin(), action.end(), dbufa);
+      MPI_Send(dbufa, control_vars, MPI_DOUBLE, i, i*2, MPI_COMM_WORLD); // send action
+      printf("send action to %d = %f  \n", 1 , action[0]);
+      float reward=dbufsrt[obs_vars];
+      terminate = false;
+      done =false;
+      if (abs(dbufsrt[obs_vars+1]-1) < 1E-3){
+        terminate=true;
+        n_ep++;
+      }if (abs(dbufsrt[obs_vars+1]-2) < 1E-3){
+        done=true;
+        n_ep++;
+      }
+      n_timestep++;
+    }
+    if ((n_ep > Nepisodes) or (n_timestep > Max_timestep)){
+      break;
+    }
+  }
 
-      MPI_Recv(dbuf, state_vars, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD, &status);
-      // MPI_Irecv(dbuf, state_vars, MPI_DOUBLE, 0, 10, MPI_COMM_WORLD, &request);
-      // MPI_Wait( &request, &status);
-      // std::vector<double> state(state_vars);
-      // for (int i=0;i<state_vars;i++) state[i]=dbuf[i];
-      std::vector<double> state(dbuf,dbuf+state_vars);
-
-      printf("recieve state from %d = %f %f %f %f %f %f \n",1, state[0] ,state[1], state[2] , state[3] ,state[4], state[5]);
-      for (int j=0; j <N_timestep; j++)
-      { 
-        // printf("recieve state from %d = %f %f %f %f %f %f \n",1, state[0] ,state[1], state[2] , state[3] ,state[4], state[5]);
-        std::vector<double> action =getAction(state,control_vars);
-   
-        // for (int i=0;i<control_vars;i++) dbufa[i]=action[i];
-        std::copy(action.begin(), action.end(), dbufa);
-        MPI_Send(dbufa, control_vars, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD); // send action
-        printf("send action to %d = %f  \n", 1 , action[0]);
-        MPI_Recv(dbufsrt, state_vars+2, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD, &status); // recieve state, reward, termination
-        // MPI_Irecv(dbufsrt, state_vars+2, MPI_DOUBLE, 0, 10, MPI_COMM_WORLD, &request);
-        // MPI_Wait( &request, &status);
-
-        // for (int i=0;i<state_vars;i++) state[i]=dbufsrt[i];
-        
-        state.insert(state.begin(),std::begin(dbufsrt),std::begin(dbufsrt)+state_vars);
-        // std::vector<double> state(dbufsrt,dbufsrt+state_vars);  //creating a new vector here will cause problem
-
-        // printf("recieve state from %d = %f %f %f %f %f %f \n",1, state[0] ,state[1], state[2] , state[3] ,state[4], state[5]);
-        float reward=dbufsrt[state_vars];
-        bool terminal = true;
-        if (abs(dbufsrt[state_vars+1]) < 1E-3){
-          terminal=false;
-        }
-        if (terminal) break;
-      }  //end of simulation loop
-    }// end of train loop
 }
 int main(int argc, char**argv)
 {
-  int nprocs;
+
   int myid;
   int n;
   MPI_Init(&argc, &argv);
@@ -138,7 +109,7 @@ int main(int argc, char**argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
   
   if (myid == 0) {
-    NN_run(nprocs);
+    NN_run();
   }
   else {
     env_run(myid);
