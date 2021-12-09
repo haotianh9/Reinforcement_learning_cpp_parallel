310aa0d1412acb16ee9a9b1376cca914e438ab9e
Yusheng Jiao <yusheng9559@gmail.com>
2021-12-09 13:38:51 -0800

    bug fixing2

--- RL3linkFishCppPortMerge/ppo_nn.h
+++ RL3linkFishCppPortMerge/ppo_nn.h
@@ -8,13 +8,13 @@
 
 #define PRINT_SIZES(a) cout << a[0] <<" " << a[1] << endl
 using namespace std;
-std::tuple<std::vector<double>, double> getAction(std::vector<double> observation,  int dim, PPO ppo, MemoryNN& memoryNN)
+tuple<vector<double>, double> getAction(vector<float> observation,  int dim, PPO ppo, MemoryNN& memoryNN)
 {
-    std::vector<double> action(dim);
+    // std::vector<double> action(dim);
     //should we return both the action and the log prob here?
     
     torch::Tensor observationTensor = torch::from_blob(observation.data(), {(long int)observation.size()}, torch::TensorOptions().dtype(torch::kFloat32));
-    std::cout << "HERE IS WHAT YOU SHOULD LOOK AT" << observation << '\n' 
+    cout << "HERE IS WHAT YOU SHOULD LOOK AT" << observation << '\n' 
                 << observation.data() << '\n' << observationTensor << endl;
     auto [actionTensor, logProbTensor] = ppo.select_action(observationTensor, memoryNN);
     // action[0]=observation[0]+observation[1];
